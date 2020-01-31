07e5a675cb72223e76769db33ed1eac3d0f04e13
Anisa <anisahamohamed@gmail.com>
2020-01-31 11:19:08 -0500

    Exercise 34: Accessing Elements of Arrays

---
+++ ex34.rb
@@ -0,0 +1,21 @@
+
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "These are the animals #{animals}"
+# The animal at 1.
+puts "The animal at 1 is #{animals[1]}."
+# The third (3rd) animal.
+puts " The third (3rd) animal is #{animals[0]}."
+# The first (1st) animal.
+puts "The first (1st) animal is #{animals[0]}."
+# The animal at 3.
+puts "The animal at 3 is #{animals[3]}."
+# The fifth (5th) animal.
+puts "The fifth (5th) animal is #{animals[4]}."
+# The animal at 2.
+puts "The animal at 2 is #{animals[2]}."
+# The sixth (6th) animal.
+puts "The sixth (6th) animal is #{animals[5]}." 
+# The animal at 4.
+puts "The animal at 4 is #{animals[4]}."
