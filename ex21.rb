c047cd41b5f3e85dd0a2ad7cae4696ac873bc719
Anisa <anisahamohamed@gmail.com>
2020-01-31 02:02:43 +0000

    Exercise 21: Functions Can Return Something

---
+++ ex21.rb
@@ -0,0 +1,44 @@
+def add(a, b)
+  puts "ADDING #{a} + #{b}"
+  return a + b
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+  return a - b
+end
+
+def multiply(a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+  return a * b
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+  return a / b
+end
+
+
+puts "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+
+# A puzzle for the extra credit, type it in anyway.
+puts "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+
+puts "That becomes: #{what}. Can you do it by hand?"
+def simplified_function(age, height, weight, iq)
+  simple_answer = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+  puts "this is the simplied code that gets the same answer which is #{simple_answer}"
+end
+
+simplified_function(age, height, weight, iq)
